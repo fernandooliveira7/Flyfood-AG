579ef4de0865f9b157809a7b7e239301d13db715
José Fernando de Oliveira Filho <57115855+fernandooliveira7@users.noreply.github.com>
2023-12-22 09:06:18 -0300

    Update Flyfood.py

--- Flyfood.py
+++ Flyfood.py
@@ -1,233 +1,85 @@
 import random
-import matplotlib.pyplot as plt
-import time
-random.seed(328315)
-class Pontos:
 
-    def __init__(self, numero, coord_x, coord_y):
-        self.numero = numero
-        self.coord_x = coord_x
-        self.coord_y = coord_y
+# Função para ler a matriz de um arquivo
+def read_matrix(file_name):
+    with open(file_name, 'r') as file:
+        lines = file.readlines()
+        matrix = [list(line.strip().split()) for line in lines]
+    return matrix
 
-    def __repr__(self):
-        return f'{int(self.numero)}'
+# Define o arquivo de entrada
+input_file = "matriz.txt"
 
-    def distancia(self, Pontos):
-        xDistancia = (self.coord_x - Pontos.coord_x)**2
-        yDistancia = (self.coord_y - Pontos.coord_y)**2
-        distancia = (xDistancia + yDistancia)**(1/2)
-        return distancia
+# Lê a matriz do arquivo de entrada
+matrix = read_matrix(input_file)
 
-class Route:
-    def __init__(self, rota, fitness=None):
-        self.rota = rota
-        self.fitness = fitness
+# Encontra os pontos de entrega (exceto R)
+points = []
+for i in range(len(matrix)):
+    for j in range(len(matrix[i])):
+        if matrix[i][j] not in ['0', 'R']:
+            points.append(matrix[i][j])
 
-class Population:
+# Função para calcular o custo de uma ordem de entrega
+def calculate_cost(order):
+    cost = 0
+    order = ['R'] + order + ['R']
+    for i in range(len(order) - 1):
+        x1, y1 = [index for index, row in enumerate(matrix) if order[i] in row][0], [row.index(order[i]) for row in matrix if order[i] in row][0]
+        x2, y2 = [index for index, row in enumerate(matrix) if order[i+1] in row][0], [row.index(order[i+1]) for row in matrix if order[i+1] in row][0]
+        cost += abs(x1 - x2) + abs(y1 - y2)
+    return cost
 
-    def __init__(self):
-        self.individuals = []
-
-    def add(self, solution):
-        self.individuals.append(solution.rota)
-
-    def addfrente(self, solution):
-        self.individuals.insert(0, solution)
-
-    def sort_individuals(self):
-        self.individuals.sort(key=lambda x: (route_distance(x)))
-
-    def size(self):
-        return len(self.individuals)
-
-
-def route_distance(sequence):
-    distancia_total = 0
-    for i in range(len(sequence) - 1):
-        ponto_atual = sequence[i]
-        proximo_ponto = sequence[i + 1]
-        distancia_total += (ponto_atual.distancia(proximo_ponto))
-    return distancia_total
-
-def newroutes(locais):
-    rota = random.sample(locais, len(locais))
-    return rota
-
-def initial_population(locais, len_pop):
-    population = Population()
-    while population.size() < len_pop:
-        lista_temporaria = newroutes(locais)
-        lista_temporaria.insert(0, ponto_inicial)
-        lista_temporaria.append(ponto_inicial)
-        if lista_temporaria not in population.individuals:
-            population.add(Route(lista_temporaria, route_distance(lista_temporaria)))
+# Função para criar uma população inicial de soluções
+def generate_initial_population(size):
+    population = [random.sample(points, len(points)) for _ in range(size)]
     return population
 
-def torneio(population):
-    choices = random.choices(population.individuals, k=2)
-    best_ind = None
-    best_fit = float('inf')
+# Função para selecionar os melhores indivíduos com base no custo
+def select_best_individuals(population, num_parents):
+    costs = [calculate_cost(individual) for individual in population]
+    sorted_population = [x for _, x in sorted(zip(costs, population))]
+    return sorted_population[:num_parents]
 
-    for i in choices:
-        distance = route_distance(i)
-        if distance < best_fit:
-            best_fit = distance
-            best_ind = i
-    return best_ind
+# Função para cruzar dois indivíduos
+def crossover(parent1, parent2):
+    crossover_point = random.randint(1, len(parent1) - 1)
+    child = parent1[:crossover_point] + [gene for gene in parent2 if gene not in parent1[:crossover_point]]
+    return child
 
-def crossover(pai1, pai2, crossover_tax):
+# Função para mutar um indivíduo
+def mutate(individual, mutation_rate):
+    if random.random() < mutation_rate:
+        idx1, idx2 = random.sample(range(len(individual)), 2)
+        individual[idx1], individual[idx2] = individual[idx2], individual[idx1]
+    return individual
 
-    if random.random() <= crossover_tax:
+# Parâmetros do algoritmo genético
+population_size = 100
+num_generations = 100
+num_parents = 50
+mutation_rate = 0.1
 
-        partition = random.randint(1, len(pai1))
-        filho1, filho2 = pai1, pai2
-        pai1, pai2 = tuple(pai1), tuple(pai2)
+# Gera uma população inicial
+population = generate_initial_population(population_size)
 
-        for i in range(len(pai1)):
-            if i <= partition:
-                if filho1[i] != pai2[i]:
-                    substituir = filho1[i]
-                    encontrar = pai2[i]
-                    j = i
-                    while j < len(filho1):
-                        if filho1[j] == encontrar:
-                            filho1[j] = substituir
-                            j = len(filho1)
-                        j += 1
-                    filho1[i] = encontrar
-            else:
-                if filho2[i] != pai1[i]:
-                    substituir = filho2[i]
-                    encontrar = pai1[i]
-                    j = 0
-                    while j < len(filho2):
-                        if filho2[j] == encontrar:
-                            filho2[j] = substituir
-                            j = len(filho1)
-                        j += 1
-                    filho2[i] = encontrar
-    else:
-        return list(pai1).copy(), list(pai2).copy()
-    return filho1, filho2
+# Executa o algoritmo genético
+for generation in range(num_generations):
+    parents = select_best_individuals(population, num_parents)
+    offspring = []
 
+    while len(offspring) < population_size:
+        parent1, parent2 = random.sample(parents, 2)
+        child = crossover(parent1, parent2)
+        child = mutate(child, mutation_rate)
+        offspring.append(child)
 
-def mutation(individuo, taxaDeMutacao):
-    for i in range(1, len(individuo)-1):
-        if (random.random() < taxaDeMutacao):
+    population = parents + offspring
 
-            trocarCom = random.randrange(1, len(individuo))
+# Encontra a melhor solução na última geração
+best_solution = min(population, key=calculate_cost)
 
-            while trocarCom == 0 or trocarCom == len(individuo)-1:
-                trocarCom = random.randrange(1, len(individuo))
+# Imprime a ordem de entrega com o menor custo
+print(' '.join(best_solution))
 
-            ponto1 = individuo[i]
-            ponto2 = individuo[trocarCom]
-
-            individuo[i] = ponto2
-            individuo[trocarCom] = ponto1
-    return individuo
-
-def new_population(curr_population, elitism, crossover_tax, mutation_tax):
-    new_pop = Population()
-    curr_population.sort_individuals()
-    melhor_ind = []
-    for i in range(0, elitism):
-        melhor_ind.append(tuple(curr_population.individuals[i]))
-    while len(new_pop.individuals) < len(curr_population.individuals)-elitism:
-        dad1 = torneio(curr_population)
-        dad2 = torneio(curr_population)
-        f1, f2 = crossover(dad1, dad2, crossover_tax)
-        f1 = mutation(f1, mutation_tax)
-        f2 = mutation(f2, mutation_tax)
-        if f1 not in new_pop.individuals:
-            new_pop.individuals.append(f1)
-        if f2 not in new_pop.individuals and len(new_pop.individuals) < len(curr_population.individuals)-elitism:
-            new_pop.individuals.append(f2)
-    for i in range(len(melhor_ind)-1, -1, -1):
-        new_pop.addfrente(list(melhor_ind[i]))
-    return new_pop
-
-
-def algoritmoGenetico(locais, tamanhoDaPopulacaoInicial, taxadecrossover, taxaDeMutacao, numeroDeGeracoes):
-    pop = initial_population(locais, tamanhoDaPopulacaoInicial)
-    pop.sort_individuals()
-    bestrota = pop.individuals[0]
-    '''''
-    print(route_distance(bestrota))
-    X = []
-    y = []
-    for i in range(len(bestrota)):
-        X.append(bestrota[i].coord_x)
-        y.append(bestrota[i].coord_y)
-    plt.plot(X, y, 'b--o')
-    plt.title('Melhor rota | Geração 1')
-    plt.plot(color='green', marker='o', linestyle='-', linewidth=2)
-    plt.show()
-    '''
-    short_distance = []
-    #n = 2000
-    for i in range(1, numeroDeGeracoes + 1):
-        pop = new_population(pop, tamanhoElitismo, taxadecrossover, taxaDeMutacao)
-
-        pop.sort_individuals()
-        bestrota = pop.individuals[0]
-        menor_distancia = route_distance(bestrota)
-        print(menor_distancia)
-        short_distance.append(menor_distancia)
-        '''''
-        if i == n:
-            X = []
-            y = []
-            for i in range(len(bestrota)):
-                X.append(bestrota[i].coord_x)
-                y.append(bestrota[i].coord_y)
-            plt.plot(X, y, 'b--o')
-            plt.title('Melhor rota | Geração {}'.format(n))
-            plt.plot(color='green', marker='o', linestyle='-', linewidth=2)
-            plt.show()
-            n += 2000
-        
-    X = []
-    y = []
-    for i in range(len(bestrota)):
-        X.append(bestrota[i].coord_x)
-        y.append(bestrota[i].coord_y)
-    plt.plot(X, y, 'b--o')
-    plt.title('Melhor rota | Geração {}'.format(numeroDeGeracoes))
-    plt.plot(color='green', marker='o', linestyle='-', linewidth=2)
-    plt.show()
-'''
-    plt.plot(short_distance)
-    plt.ylabel('Menor distancia')
-    plt.xlabel('Geração')
-    plt.title('Menor distância vs Geração')
-    plt.tight_layout()
-    plt.show()
-    return bestrota
-
-
-#Main
-ini = time.time()
-locais = []
-arquivo = open('a280.tsp', 'r')
-for linha in arquivo.readlines():
-    linhasplit = linha.split()
-    coordenadas = []
-    coordenadas.append(linhasplit[0])
-    coordenadas.append(linhasplit[1])
-    coordenadas.append(linhasplit[2])
-    if linhasplit[0] != '1':
-        locais.append(Pontos(int(linhasplit[0]), int(linhasplit[1]), int(linhasplit[2])))
-    else:
-        ponto_inicial = (Pontos(int(linhasplit[0]), int(linhasplit[1]), int(linhasplit[2])))
-
-tamanhoDaPopulacaoInicial = 30
-tamanhoElitismo = 2
-taxaDeMutacao = 0.01
-taxadecrossover = 0.6
-numeroDeGeracoes = 10000
-melhorrota = algoritmoGenetico(locais, tamanhoDaPopulacaoInicial, taxadecrossover, taxaDeMutacao, numeroDeGeracoes)
-#print(melhorrota)
-fim = time.time()
-print("tempo de execução:", fim-ini)
 
